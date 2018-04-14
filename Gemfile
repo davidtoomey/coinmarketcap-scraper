4c328528fba8d1b23a39c8d86e8dbcde5e6ffbda
David Toomey <d.toomey@temple.edu>
2018-04-14 05:22:44 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'pry-rails'
+gem 'rb-readline'
+gem 'watir', '~> 5.0'
+gem 'nokogiri'
---
+++ coinScraper.rb
@@ -0,0 +1,24 @@
+require 'bundler/setup'
+
+require 'pry-rails'
+
+require 'watir'
+
+require 'nokogiri'
+
+require 'open-uri'
+
+loop do |updat|
+  doc = Nokogiri::HTML.parse(open("https://coinmarketcap.com"))
+  coinsTable = doc.css("table").sort { |x, y| y.css("tr").count <=> x.css("tr").count }.first
+  rows = coinsTable.css("tr")
+  rows = rows.select { |row| row.css("th").empty? }
+  data = rows.map do |row|
+    [row.at_css("td:nth-child(2)").text, row.at_css("td:nth-child(100)")] + [row.at_css("td:nth-child(4)").text, row.at_css("td:nth-child(100)")] + [row.at_css("td:nth-child(7)").text, row.at_css("td:nth-child(100)")]
+  end
+  sleep 5
+  open('data.txt', 'w') do |f|
+    f.puts data
+  end
+  puts "Updated."
+end
