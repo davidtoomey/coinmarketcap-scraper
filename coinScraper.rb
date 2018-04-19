68e9484fa76dccd7d350594da82fc2ac78b11a5c
David Toomey <d.toomey@temple.edu>
2018-04-18 20:57:39 -0400

    cleaned up whitespaces in output

--- coinScraper.rb
+++ coinScraper.rb
@@ -10,11 +10,11 @@ require 'open-uri'
 
 loop do |updat|
   doc = Nokogiri::HTML.parse(open("https://coinmarketcap.com"))
-  coinsTable = doc.css("table").sort { |x, y| y.css("tr").count <=> x.css("tr").count }.first
+  coinsTable = doc.css("table").sort { |x, y| y.css("tr").count.lstrip <=> x.css("tr").count.rstrip }.first
   rows = coinsTable.css("tr")
   rows = rows.select { |row| row.css("th").empty? }
   data = rows.map do |row|
-    [row.at_css("td:nth-child(2)").text + row.at_css("td:nth-child(4)").text.gsub(/\s+/, "")]
+    [row.at_css("td:nth-child(2)").text + row.at_css("td:nth-child(4)").text.gsub(/\s+/, "")] + [row.at_css("td:nth-child(7)").text]
   end
   open('data.txt', 'w') do |f|
     f.puts data
